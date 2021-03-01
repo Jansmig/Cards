9dce244fa54616f922d0505f59ceff737053f2fa
Jan Smigielski <jan.smigielski@brainworks.pl>
2021-03-01 13:16:25 +0100

    cards final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
-}
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'Weasel',
+      content: 'I saw a neat tree today.'
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'Mountain-Geek',
+      content: 'Snowy mountain rules.'
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biker123',
+      content: 'best hobby ever!'
+    }
+  ];
+} 
